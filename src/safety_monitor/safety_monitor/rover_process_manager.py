c0ffc54791d8cccf9b21d04cdc848d9ec979c82d
NickWey111 <niwe7487@colorado.edu>
2026-04-03 15:27:43 -0600

    feat: add RoverProcessManager with tests

---
+++ src/safety_monitor/safety_monitor/rover_process_manager.py
@@ -0,0 +1,41 @@
+import subprocess
+from typing import Optional
+
+
+class RoverProcessManager:
+    """Spawns and manages the rover_node OS process."""
+
+    def __init__(self, connection_string: str, baud_rate: int):
+        self._connection_string = connection_string
+        self._baud_rate = int(baud_rate)
+        self._proc: Optional[subprocess.Popen] = None
+
+    def spawn(self) -> None:
+        """Start rover_node as a child subprocess."""
+        self._proc = subprocess.Popen([
+            'ros2', 'run', 'robo_rover', 'rover_node',
+            '--ros-args',
+            '-p', f'connection_string:={self._connection_string}',
+            '-p', f'baud_rate:={self._baud_rate}',
+        ])
+
+    def kill(self) -> None:
+        """Terminate rover_node. SIGTERM first, SIGKILL after 2s if needed."""
+        if self._proc is None:
+            return
+        self._proc.terminate()
+        try:
+            self._proc.wait(timeout=2.0)
+        except subprocess.TimeoutExpired:
+            self._proc.kill()
+            try:
+                self._proc.wait()
+            except subprocess.TimeoutExpired:
+                pass
+        self._proc = None
+
+    def is_alive(self) -> bool:
+        """Return True if the subprocess is currently running."""
+        if self._proc is None:
+            return False
+        return self._proc.poll() is None

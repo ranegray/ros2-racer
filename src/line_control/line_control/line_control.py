dc28741db227e72ff7316a5bcced9f5e9fa05e4a
Justin Costa <justincosta10@gmail.com>
2026-04-15 17:48:00 -0600

    fixing p control drive speed

--- src/line_control/line_control/line_control.py
+++ src/line_control/line_control/line_control.py
@@ -21,7 +21,7 @@ class LineControlNode(Node):
         self.image_center_x = self.image_width / 2.0
         self.target_x = 400.0         # pixel column where a centered line appears (camera is mounted off-center)
         self.steering_kp = 1.0        # proportional gain on normalized pixel offset
-        self.drive_speed = 0.2        # fixed forward speed (m/s)
+        self.drive_speed = 0.4        # fixed forward speed (m/s)
         self.goal_timeout = 1.0       # stop if no goal received for this long (s)
 
         self.get_logger().info("Line Control Node has started!")

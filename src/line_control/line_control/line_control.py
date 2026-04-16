f7a5e85c6f322d4dc093904ec33e40c9c484b160
Justin Costa <justincosta10@gmail.com>
2026-04-15 18:09:43 -0600

    bumping up steering gain

--- src/line_control/line_control/line_control.py
+++ src/line_control/line_control/line_control.py
@@ -20,7 +20,7 @@ class LineControlNode(Node):
         self.image_width = 640
         self.image_center_x = self.image_width / 2.0
         self.target_x = 400.0         # pixel column where a centered line appears (camera is mounted off-center)
-        self.steering_kp = 0.75       # proportional gain on normalized pixel offset
+        self.steering_kp = 1.25       # proportional gain on normalized pixel offset
         self.drive_speed = 0.3        # fixed forward speed (m/s)
         self.goal_timeout = 1.0       # stop if no goal received for this long (s)
 

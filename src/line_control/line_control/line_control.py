dd7b56dcaa1f93fb9cedd3bf3f1848e99da832d7
Nick <nicholas.wey111@gmail.com>
2026-04-20 12:04:33 -0600

    d term is 2.0 p is 1.6 also looking farther ahead

--- src/line_control/line_control/line_control.py
+++ src/line_control/line_control/line_control.py
@@ -23,7 +23,7 @@ class LineControlNode(Node):
         self.image_width = 640
         self.image_center_x = self.image_width / 2.0
         self.target_x = 400.0  # pixel column where a centered line appears (camera is mounted off-center)
-        self.steering_kp = 1.2   # proportional gain on follow-point offset
+        self.steering_kp = 1.6   # proportional gain on follow-point offset
         self.steering_kd = 2.0   # derivative gain on follow-point offset
         self.turn_steering_kp = (
             2.65  # proportional gain on turn-point offset (override)

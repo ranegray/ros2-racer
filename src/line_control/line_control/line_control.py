0c4684183156233fa1dcb6f763b01b3397d1cc71
Justin Costa <justincosta10@gmail.com>
2026-04-19 23:00:53 -0600

    tuning

--- src/line_control/line_control/line_control.py
+++ src/line_control/line_control/line_control.py
@@ -25,8 +25,8 @@ class LineControlNode(Node):
         self.target_x = 400.0  # pixel column where a centered line appears (camera is mounted off-center)
         self.steering_kp = 1.4  # proportional gain on follow-point offset
         self.turn_steering_kp = 2.65  # proportional gain on turn-point offset (override)
-        self.base_speed = 0.35  # forward speed when following (m/s)
-        self.turn_speed = 0.35  # forward speed when turning (m/s)
+        self.base_speed = 0.37  # forward speed when following (m/s)
+        self.turn_speed = 0.37  # forward speed when turning (m/s)
         self.goal_timeout = 1.0  # stop if no goal received for this long (s)
 
         self.get_logger().info("Line Control Node has started!")

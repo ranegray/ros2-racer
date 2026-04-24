c33a7038a71cbf9d5b30fc0d25dbd53a1bde23e0
Nick <nicholas.wey111@gmail.com>
2026-04-24 15:27:37 -0600

    lower the turn speed

--- src/autonomy/autonomy/wall_follower_node.py
+++ src/autonomy/autonomy/wall_follower_node.py
@@ -95,7 +95,7 @@ SIGN          = -1.0
 STEERING_TRIM =  0.0  # positive = trim right, negative = trim left (mechanical bias correction)
 MAX_STEER     =  2.0  # ±2.0 = full servo lock
 BASE_SPEED    =  0.40  # m/s nominal
-TURN_SPEED    =  0.33  # m/s minimum (wall-lost / corners)
+TURN_SPEED    =  0.31  # m/s minimum (wall-lost / corners)
 SPEED_ALPHA_SCALE_DEG = 90.0  # alpha (deg) at which speed hits TURN_SPEED
 
 # Battery voltage scaling

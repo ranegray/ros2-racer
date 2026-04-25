a47fbfe89f1ae088bc2b64146799fca62f184da8
Justin Costa <justincosta10@gmail.com>
2026-04-25 14:40:08 -0600

    edited save file

--- src/image_processing/save_image_stream.py
+++ src/image_processing/save_image_stream.py
@@ -9,14 +9,16 @@ import os
 W, H, FPS = 640, 480, 30  # width, heigh, frames per second
 
 pipe = rs.pipeline()
+pipe.stop()
 cfg  = rs.config()
 cfg.enable_stream(rs.stream.color, W, H, rs.format.bgr8, FPS)
 j = 0
 path = os.getcwd()
 dirname = os.path.join(path, 'r/run{}'.format(j))
-while not os.path.exists(dirname):          
+while os.path.exists(dirname):          
    j = j+1
    dirname = os.path.join(path, 'r/run{}'.format(j))
+os.makedirs(dirname)
 print("Starting pipeline…")
 pipe.start(cfg)
 frames = pipe.wait_for_frames(5000)   # 5s timeout
@@ -29,10 +31,10 @@ def update(self):
         color  = frames.get_color_frame()
         img = np.asanyarray(color.get_data())[:,:,::-1]  # already BGR
         im.set_data(img)
-        imgname = 'r/Cameraframe{x}.png'.format(i))
+        imgname = 'r/Cameraframe{x}.png'.format(i)
         while not os.path.exists(os.path.join(dirname,imgname)):                 
             i = i+1            
-            imgname = 'r/Cameraframe{x}.png'.format(i))
+            imgname = 'r/Cameraframe{x}.png'.format(i)
         image.imsave(os.path.join(dirname, imgname), img)
         return im
 

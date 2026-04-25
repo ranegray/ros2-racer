01ddf1b05cb8e1726782e59a86dd6142c9302992
SenaytW <tyanessedlow@gmail.com>
2026-04-25 14:49:30 -0600

    fixed broken file

--- src/image_processing/desktop_stream.py
+++ src/image_processing/desktop_stream.py
@@ -2,8 +2,7 @@ import pyrealsense2 as rs
 import matplotlib.pyplot as plot
 import numpy as np
 from matplotlib.animation import FuncAnimation
-import matplotlib.image as image
-import os
+
 
 
 W, H, FPS = 640, 480, 30  # width, heigh, frames per second
@@ -11,29 +10,18 @@ W, H, FPS = 640, 480, 30  # width, heigh, frames per second
 pipe = rs.pipeline()
 cfg  = rs.config()
 cfg.enable_stream(rs.stream.color, W, H, rs.format.bgr8, FPS)
-path = os.getcwd()
 print("Starting pipeline…")
 pipe.start(cfg)
 frames = pipe.wait_for_frames(5000)   # 5s timeout
 color  = frames.get_color_frame()
 img = np.asanyarray(color.get_data())[:,:,::-1]  # already BGR
 im = plot.imshow(img)
-i = 0
-j = 0
+
 def update(self):
         frames = pipe.wait_for_frames(5000)   # 5s timeout
         color  = frames.get_color_frame()
         img = np.asanyarray(color.get_data())[:,:,::-1]  # already BGR
         im.set_data(img)
-        
-        i = i+1
-        imgname = 'r/Cameraframe{x}.png'.format(i)
-        dirname = os.path.join(path, 'r/run{}'.format(j))
-        while not os.path.exists(dirname):          
-           j = j+1
-           dirname = os.path.join(path, 'r/run{}'.format(j))
-        os.makedirs(dirname)
-        image.imsave(os.path.join(dirname, imgname), img)
         return im
 
 

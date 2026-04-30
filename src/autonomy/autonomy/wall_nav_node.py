6af16656a362527ac1481a04927eb13955840d83
Nick <nicholas.wey111@gmail.com>
2026-04-30 11:45:19 -0600

    avoid: kp 0.3->0.2, kd 2.0->2.8

--- src/autonomy/autonomy/wall_nav_node.py
+++ src/autonomy/autonomy/wall_nav_node.py
@@ -231,8 +231,8 @@ class WallNavNode(Node):
         self.declare_parameter("avoid_confirm_scans", 2)          # scans to confirm
         self.declare_parameter("front_avoid_deg", 25.0)           # diagonal angle (deg)
         self.declare_parameter("front_avoid_min_asym", 0.15)      # m -- ignore below this
-        self.declare_parameter("front_avoid_kp", 0.3)
-        self.declare_parameter("front_avoid_kd", 2.0)
+        self.declare_parameter("front_avoid_kp", 0.2)
+        self.declare_parameter("front_avoid_kd", 2.8)
         self.declare_parameter("front_avoid_d_alpha", 0.8)        # D-term low-pass
         self.declare_parameter("front_avoid_max_diag_mult", 3.0)  # diagonal > N*fwd = gap
         self.declare_parameter("front_avoid_abs_gap_thresh", 3.5) # m -- absolute gap limit
